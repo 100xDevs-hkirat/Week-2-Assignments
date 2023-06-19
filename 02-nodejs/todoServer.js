e87230b14158448d66a28708a0a766986f365fec
asbar <asbar03KK@gmail.com>
2023-06-19 16:29:42 +0530

    compleated todo

--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -39,11 +39,89 @@
 
   Testing the server - run `npm run test-todoServer` command in terminal
  */
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+let todos = [];
 
 app.use(bodyParser.json());
+function creteId(arr) {
+  if (arr.length) {
+    return arr[arr.length - 1].id;
+  } else {
+    return 0;
+  }
+}
 
+app.get("/todos", (req, res) => {
+  res.status(200).send(todos);
+});
+app.get("/todos/:id", (req, res) => {
+  const idparams = req.params["id"];
+  let idTodos = todos.find((todo) => {
+    return todo.id == idparams;
+  });
+  // console.log(idparams);
+  if (idTodos == undefined) {
+    return res.sendStatus(404);
+  }
+  // console.log(idTodos);
+  res.status(200).send(idTodos);
+});
+app.post("/todos", (req, res) => {
+  let todoTitle = req.body.title;
+  let todoDescription = req.body.description;
+  let id = creteId(todos);
+  let todoObject = {
+    id: id + 1,
+    title: todoTitle,
+    description: todoDescription,
+  };
+
+  todos.push(todoObject);
+  // console.log(todos);
+  res.status(201).json({ id: todoObject.id });
+});
+app.put("/todos/:id", (req, res) => {
+  const idPut = req.params["id"];
+  let indexOfTodo;
+  let todoId = todos.find((todo, index) => {
+    indexOfTodo = Number(index);
+    return todo.id == idPut;
+  });
+  // console.log(idPut);
+  if (todoId === undefined) {
+    return res.sendStatus(404);
+  }
+  let todoTitle = req.body.title;
+  let todoDescription = req.body.description;
+  let todoObject = {
+    id: Number(idPut),
+    title: todoTitle,
+    description: todoDescription,
+  };
+  todos[indexOfTodo] = todoObject;
+  // console.log(todos);
+  res.sendStatus(200);
+});
+app.delete("/todos/:id", (req, res) => {
+  const id = req.params["id"];
+  let indexDel;
+  let toDele = todos.find((todo, index) => {
+    todo.id === id;
+    indexDel = index;
+  });
+  todos = todos
+    .slice(0, indexDel)
+    .concat(todos.slice(indexDel, todos.length - 1));
+  // console.log(todos);
+  res.sendStatus(200);
+});
+app.get("/*", (req, res) => {
+  res.sendStatus(404);
+});
+// app.listen(3000, () => {
+//   console.log("Listining in 3000");
+// });
 module.exports = app;
