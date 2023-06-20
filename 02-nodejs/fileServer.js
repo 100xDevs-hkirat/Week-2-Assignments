e38aaa696dd6360e2212d5945cf4da55f1344cea
TG <tanaygodse@gmail.com>
2023-06-20 08:22:26 +0530

    Submission

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -16,10 +16,41 @@
 
     Testing the server - run `npm run test-fileServer` command in terminal
  */
+const { warn } = require('console');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const port = 3000
 
+
+const FILES_LIST = []
+fs.readdirSync('./files').forEach(file=>{
+	FILES_LIST.push(file)	
+})
+
+app.get('/files', (req, res) => {
+  fs.readdir(path.join(__dirname, './files/'), (err, files) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to retrieve files' })
+    }
+    res.json(files)
+  })
+})
+
+app.get('/file/:filename', (req, res) => {
+  const filepath = path.join(__dirname, './files/', req.params.filename);
+
+  fs.readFile(filepath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(404).send('File not found')
+    }
+    res.send(data)
+  })
+})
+
+app.get('*', (req, res)=>{
+  res.status(404).send("Route not found");
+})
 
 module.exports = app;
