be32250488c497e9728bb2ce7c19a87854c283a5
daksh <daksh21stcentury@gmail.com>
2023-06-22 00:03:03 +0530

    Basic Todo implementation completed

--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -46,4 +46,127 @@ const app = express();
 
 app.use(bodyParser.json());
 
+let todos = []
+function createTodo(req,res){
+    /**
+     * This function handles the incoming request to create a todo
+     * @param {req} the incoming request
+     * @param {res} the outgoing response that we will set
+     * @returns A JSON object with the data and success
+     */
+    let results = {}
+    let response = null
+    try {
+        const id = Date.now()
+        const todo = {...req.body, id: id}
+        if (todo.title && todo.description) {
+            todos.push(todo)
+            results.success = true
+            results.data = "Todo added successfully"
+            results.id = id
+            response = res.status(200).json(results)
+        }else {
+            results.success = false
+            results.data = "Title or description missing"
+            response = res.status(400).json(results)
+        }
+        return response
+    }catch (e) {
+        results.success = false
+        results.error = "Not able to add Todo"
+        return res.status(500).json(results)
+    }
+}
+
+function getAllTodos(req,res) {
+    let results = {}
+    try {
+        results.success = true
+        results.data = todos
+        return  res.status(200).json(results)
+    }catch (e) {
+        results.success = false
+        results.error = "Not able to get all todos"
+        return  res.status(500).json(results)
+    }
+}
+
+function getTodo(req,res) {
+    let results = {}
+    let response = null
+    try {
+        const id = +req.param.id
+        if (!id) {
+            results.data = false;
+            results.success = "Please provide an ID of TODO"
+            response = res.status(401).json(results)
+        }else {
+            const todo = todos.filter(todo => todo.id === id)
+            if (!todo) {
+                results.success = false
+                results.data = "No TODO found with the ID"
+                response = res.status(404).send(results)
+            }else {
+                results.success = true
+                results.data = todo
+                response = res.status(200).json(results)
+            }
+        }
+        return response
+    }catch (e) {
+        results.success = false
+        results.data = "Error getting todo"
+        return res.status(500).json(results)
+
+    }
+}
+
+function updateTodo(req,res) {
+    let results = {}
+    let response = null
+    try{
+        const updatedTodo = {...req.body}
+        const todoIndex = todos.findIndex(todo => todo.id === req.param.id)
+        if(todoIndex){
+            todos[todoIndex] = {...todos[todoIndex], ...updatedTodo}
+            results.success = true
+            results.data = "Updated Todo successfully"
+            response = res.status(200).json(results)
+        }else {
+            results.success = false
+            results.data = "Can not find a todo with the given ID"
+            response = res.status(404).json(results)
+        }
+        return response
+    }catch (e) {
+        results.success = false
+        results.error = "Could not update todo with the given ID"
+        return res.status(500).json(results)
+    }
+}
+
+function deleteTodo(req,res) {
+    let results = {}
+    try{
+        const updatedTodos = todos.filter(todo => todo.id !== req.param.id)
+        todos = [...updatedTodos]
+        results.success = true
+        results.data = "Deleted the todo successfully"
+        return res.status(200).json(results)
+    }catch (e) {
+        results.success = false
+        results.data = "Error while deleting the todo"
+        return res.status(404).json(results)
+    }
+}
+app.post('/todos', createTodo)
+app.get('/todos', getAllTodos)
+app.get('/todos/:id', getTodo)
+app.put('/todos/:id', updateTodo)
+app.delete('/todos/:id', deleteTodo)
+app.use((req,res) => res.status(404))
+app.listen(3000, () => {
+    console.log('Application running on port 3000..')
+})
+
 module.exports = app;
