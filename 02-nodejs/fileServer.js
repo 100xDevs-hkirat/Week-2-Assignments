24a224f2198534be0751462a18e3dca485486d39
Mudit <muditert34@gmail.com>
2023-06-21 22:00:12 +0530

    fileServer.js added

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -9,7 +9,8 @@
 
   2. GET /file/:filename - Returns content of given file by name
      Description: Use the filename from the request path parameter to read the file from `./files/` directory
-     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
+     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. 
+     Should return `File not found` as text if file is not found
      Example: GET http://localhost:3000/file/example.txt
 
     - For any other route not defined in the server return 404
@@ -20,6 +21,37 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+
+app.get('/files', (req, res) => {
+  const directory = path.join(__dirname, 'files');
+  fs.readdir(directory, (err, data) => {
+    if (err) {
+      res.status(500).send('Error retrieving the files');
+    } else {
+      res.json({ data });
+    }
+  })
+});
+
+app.get('/file/:filename', (req, res) => {
+
+  const directory = path.join(__dirname, 'files');
+  const filename = req.params.filename;
+  const file = path.join(directory, filename);
+
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      res.status(404).send('File not found')
+    } else {
+      res.status(200).send(data);
+    }
+  })
+
+})
+
+app.use((req, res) => {
+  res.status(404).send('Route not found');
+});
 
 
 module.exports = app;
