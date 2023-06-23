c3364c0509147ee6028b0623c9370a37fd9f31b4
Himanshu Yadav <78066262+Himanshu-holmes@users.noreply.github.com>
2023-06-23 15:59:52 +0530

    Update fileServer.js
    run `npx jest ./tests/fileServer.test.js ` instead of  `npm run test-fileServer` command in terminal

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -15,7 +15,7 @@
 
     - For any other route not defined in the server return 404
 
-    Testing the server - run `npm run test-fileServer` command in terminal
+    Testing the server - run `npx jest ./tests/fileServer.test.js ` instead of  `npm run test-fileServer` command in terminal
  */
     const express = require('express');
     const PORT = 3001;
