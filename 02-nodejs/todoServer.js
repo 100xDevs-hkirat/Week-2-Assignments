7d64fb0a257124d0e683e2db2c42d4ddf381f0fc
Akash Singh <developer.akash8@gmail.com>
2023-06-23 22:18:47 +0530

    Implement API endpoints for todo server

--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -39,11 +39,69 @@
 
   Testing the server - run `npm run test-todoServer` command in terminal
  */
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(bodyParser.json());
+
+const todos = [];
+
+app.get("/todos", (req, res) => {
+  res.send(todos);
+});
+
+app.get("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todo = todos.filter((t) => t.id === id);
+
+  if (todo.length === 0) {
+    res.status(404).send();
+  } else {
+    res.json(todo[0]);
+  }
+});
+
+app.post("/todos", (req, res) => {
+  const todo = req.body;
+  todo["id"] = Math.floor(Math.random() * 1000000);
+
+  todos.push(todo);
+
+  res.status(201).json(todo);
+});
+
+app.put("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todoIndex = todos.findIndex((t) => t.id === id);
+
+  if (todoIndex !== -1) {
+    todos[todoIndex].title = req.body.title;
+    todos[todoIndex].description = req.body.description;
+    res.json(todos[todoIndex]);
+  } else {
+    res.status(404).send("Not Found");
+  }
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todoIndex = todos.findIndex((t) => t.id === id);
+
+  if (todoIndex !== -1) {
+    todos.splice(todoIndex, 1);
+    res.status(200).send("Successfully deleted");
+  } else {
+    res.status(404).send("Not Found");
+  }
+});
+
+function handleOtherRoutes(req, res, next) {
+  res.status(404).send("Route not found");
+}
+
+app.use(handleOtherRoutes);
+
 
 module.exports = app;
