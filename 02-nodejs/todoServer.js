971f84ae8ffc8c207b3edbd2934fa251daa5251a
Anand Raj Singh <singhpariharanandraj@gmail.com>
2023-06-23 21:26:09 +0530

    Completed Assignments

--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -46,4 +46,57 @@ const app = express();
 
 app.use(bodyParser.json());
 
+let todos = [];
+
+app.get('/todos', (req, res) => {
+  res.json(todos);
+})
+
+app.get('/todos/:id', (req, res) => {
+  const todo = todos.find(t => t.id === parseInt(req.params.id));
+  if (!todo) {
+    res.status(404).send();
+  } else {
+    res.json(todo);
+  }
+})
+
+app.post('/todos', (req, res) => {
+  const newToDo = {
+    id: Math.floor(Math.random() * 1000000),
+    title: req.body.title,
+    description: req.body.description
+  }
+  todos.push(newToDo);
+  res.status(201).json(newToDo);
+})
+
+app.put('/todos/:id', (req, res) =>{
+  const toDoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
+  if (toDoIndex === -1) {
+    res.status(404).send();
+  } else {
+    todos[toDoIndex].title = req.body.title;
+    todos[toDoIndex].description = req.body.description;
+    res.json(todos[toDoIndex]);
+  }
+})
+
+app.delete('/todos/:id', (req, res) => {
+  const toDoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
+  if (toDoIndex === -1) {
+    res.status(404).send();
+  } else {
+    todos.splice(toDoIndex, 1);
+    res.status(200).send();
+  }
+});
+
+app.use((req, res, next) => {
+  res.status(404).send();
+});
+
 module.exports = app;
+// app.listen(3000, () => {
+//   console.log(`Server Running on port 3000`);
+// });
