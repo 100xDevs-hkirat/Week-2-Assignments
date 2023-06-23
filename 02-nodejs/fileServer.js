0a01e5bfdb3e11741f2f15f66e01952a97dca054
mahesh2399 <maheshlucky9932@gmail.com>
2023-06-23 07:58:19 -0400

    file server completed

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -16,10 +16,50 @@
 
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const fsProm = require("fs/promises");
+const path = require("path");
+// const bodyParser = require('body-parser')
 const app = express();
 
+// app.use(bodyParser.json())
+
+app.get("/files", (req, res, next) => {
+  // fsProm
+  //   .readdir("./files")
+  //   .then((data) => res.send(data))
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.status(500).send({ msg: "could not read the directory" });
+  //   });
+
+  fs.readdir("./files", "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).send("Internal server occured");
+    }
+    res.send(data);
+  });
+});
+
+app.get("/file/:fileName", (req, res, next) => {
+  const fileName = req.params.fileName;
+  // console.log(fileName);
+  fsProm
+    .readFile("./files/" + fileName, "utf-8")
+    .then((data) => res.send(data))
+    .catch((err) => {
+      console.log(err);
+      res.status(404).send("File not found");
+    });
+});
+
+app.get("/*", (req, res, next) => {
+  res.status(404).send("Route not found");
+});
+
+// app.listen(3000, () => {
+//   console.log("node is running at port 3000");
+// });
 
 module.exports = app;
