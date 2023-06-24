4dcc4ee0a82f85a61d40bd55515d760df59b728e
Sushil <sjsushil09@gmail.com>
2023-06-24 13:18:16 +0530

    Week2Assignmnent - Solution

--- 02-nodejs/authenticationServer.js
+++ 02-nodejs/authenticationServer.js
@@ -1,9 +1,9 @@
 /**
-  You need to create a HTTP server in Node.js which will handle the logic of an authentication server.
-  - Don't need to use any database to store the data.
+ You need to create a HTTP server in Node.js which will handle the logic of an authentication server.
+ - Don't need to use any database to store the data.
 
-  - Save the users and their signup/login data in an array in a variable
-  - You can store the passwords in plain text (as is) in the variable for now
+ - Save the users and their signup/login data in an array in a variable
+ - You can store the passwords in plain text (as is) in the variable for now
 
   The expected API endpoints are defined below,
   1. POST /signup - User Signup
@@ -30,8 +30,90 @@
  */
 
 const express = require("express")
-const PORT = 3000;
+const bodyParser = require("body-parser");
+const PORT = 3010;
 const app = express();
-// write your logic here, DONT WRITE app.listen(3000) when you're running tests, the tests will automatically start the server
+// write your logic here, DON'T WRITE app.listen(3000) when you're running tests, the tests will automatically start the server
 
 module.exports = app;
+app.use(bodyParser.json());
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+let isAdminUser = false;
+const USERS = [];
+
+const {v4: uuidv4} = require('uuid');
+
+function generateUniqueId() {
+    return uuidv4();
+}
+
+//This check whether the logged in user is an admin user or not
+app.use((req, res, next) => {
+    let email;
+    let password;
+    if (req.headers.email)
+        email = req.headers.email;
+    if (req.headers.password)
+        password = req.headers.password;
+    if (IsValidCredententials(email, password)) {
+        isAdminUser = true;
+    }
+    next();
+});
+
+function IsValidCredententials(email, password) {
+    for (const user of USERS) {
+        if (user.email === email && user.password === password) {
+            return true;
+        }
+    }
+    return false;
+}
+
+app.post("/signup", (req, res) => {
+    const {email, password, firstName, lastName} = req.body;
+    const isExistingUser = USERS.find(user=>user.email===email);
+    if (isExistingUser) {
+        res.status(400).send("Bad Request");
+    } else {
+        const uniqueId = generateUniqueId();
+        USERS.push({email: email, password: password, firstName: firstName, lastName: lastName, id: uniqueId});
+        res.status(201).send("Signup successful");
+        console.log(USERS);
+    }
+});
+
+app.post("/login", (req, res) => {
+    const {email, password} = req.body;
+    let userFound = false;
+    for (const user of USERS) {
+        if (user.email === email && user.password === password) {
+            userFound = true;
+            res.send({email: email, firstName: user.firstName, lastName: user.lastName, authToken: generateUniqueId()});
+        }
+    }
+    if (!userFound)
+        res.status(401).send("Unauthorized");
+});
+
+app.get("/data", (req, res) => {
+    if (isAdminUser) {
+        const users = USERS.map(user => ({
+            email: user.email,
+            firstName: user.firstName,
+            lastName: user.lastName
+        }));
+        isAdminUser = !isAdminUser;
+        res.send({users: users});
+    } else {
+        res.status(401).send("Unauthorized");
+    }
+});
+
+app.all("*",(req, res)=>{
+    res.status(404).send("Route Not found");
+})
+
--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -1,25 +1,69 @@
 /**
-  You need to create an express HTTP server in Node.js which will handle the logic of a file server.
-  - Use built in Node.js `fs` module
+ You need to create an express HTTP server in Node.js which will handle the logic of a file server.
+ - Use built in Node.js `fs` module
 
-  The expected API endpoints are defined below,
-  1. GET /files - Returns a list of files present in `./files/` directory
-    Response: 200 OK with an array of file names in JSON format.
-    Example: GET http://localhost:3000/files
+ The expected API endpoints are defined below,
+ 1. GET /files - Returns a list of files present in `./files/` directory
+ Response: 200 OK with an array of file names in JSON format.
+ Example: GET http://localhost:3000/files
 
-  2. GET /file/:filename - Returns content of given file by name
-     Description: Use the filename from the request path parameter to read the file from `./files/` directory
-     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
-     Example: GET http://localhost:3000/file/example.txt
+ 2. GET /file/:filename - Returns content of given file by name
+ Description: Use the filename from the request path parameter to read the file from `./files/` directory
+ Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
+ Example: GET http://localhost:3000/file/example.txt
 
-    - For any other route not defined in the server return 404
+ - For any other route not defined in the server return 404
 
-    Testing the server - run `npm run test-fileServer` command in terminal
+ Testing the server - run `npm run test-fileServer` command in terminal
  */
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const port = 3090;
 
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
+
+app.get("/files", (req, res) => {
+    const filesInDirectory = [];
+    fs.readdir(__dirname + "\\files", (error, files) => {
+        if (error) {
+            res.status(500).send("Internal Server error");
+        } else {
+            files.forEach(file => {
+                filesInDirectory.push({file});
+            });
+        }
+        res.json(filesInDirectory);
+    });
+});
+
+app.get("/file/:filename", (req, res) => {
+    const fileName = req.params.filename;
+    const filePath = __dirname + "\\files";
+    fs.readdir(filePath, (error, files) => {
+        if (error) {
+            res.status(500).send("Internal server error.");
+        } else {
+            if (files.includes(fileName)) {
+                fs.readFile(filePath + `\\${fileName}`, 'utf8', (error, data) => {
+                    if (error) {
+                        console.log(error.message)
+                    } else {
+                        res.send(data);
+                    }
+                });
+            } else {
+                res.status(404).send('File not found');
+            }
+        }
+    });
+});
+
+app.all("*", (req, res) => {
+    res.status(404).send("Route not found");
+})
 
 module.exports = app;
