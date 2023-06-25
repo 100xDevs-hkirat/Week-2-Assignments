43ec4c447274aa7995321008ee705f3d46d6648a
Monish <20051015@kiit.ac.in>
2023-06-25 19:26:13 +0530

    week 2 assignment done

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -16,10 +16,71 @@
 
     Testing the server - run `npm run test-fileServer` command in terminal
  */
+
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
 
+const dirPath = './files/'
+
+function listFiles() {
+  return new Promise((resolve, reject) => {
+    fs.readdir(dirPath, (err, files) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(files);
+    })
+  })
+}
+
+function readFileContent(filePath) {
+  return new Promise((resolve, reject) => {
+  })
+}
+
+// task 1
+app.get('/files', (req, res) => {
+  const result = listFiles();
+
+  result
+    .then((files) => {
+      res.status(200).json(files);
+    })
+    .catch((err) => {
+      res.status(500).send(err + 'Failed to retrieve files');
+    })
+
+})
+
+// task 2
+app.get('/file/:filename', (req, res) => {
+  const fileName = req.params.filename;
+  const filePath = `./files/${fileName}`;
+
+  const readableStream = fs.createReadStream(filePath, 'utf-8');
+
+  readableStream.on('error', (err) => {
+    if (err.code === 'ENOENT') {
+      res.status(404).send('File not found');
+    } else {
+      res.status(500).send('Internal server error');
+    }
+  })
+
+  res.status(200);
+  readableStream.pipe(res);
+
+})
+
+app.use((req, res, next) => {
+  res.status(404).send('Route not found');
+});
+
+// app.listen(5000, () => {
+//   console.log('server on');
+// })
 
 module.exports = app;
