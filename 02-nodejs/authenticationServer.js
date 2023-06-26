e91d35a531279921b3155d978f395462421fc8be
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-06-26 22:03:46 +0530

    authenticateServer

--- 02-nodejs/authenticationServer.js
+++ 02-nodejs/authenticationServer.js
@@ -14,14 +14,14 @@
 
   2. POST /login - User Login
     Description: Gets user back their details like firstname, lastname and id
-    Request Body: JSON object with *- and password fields.
+    Request Body: JSON object with username and password fields.
     Response: 200 OK with an authentication token in JSON format if successful, or 401 Unauthorized if the credentials are invalid.
     Example: POST http://localhost:3000/login
 
   3. GET /data - Fetch all user's names and ids from the server (Protected route)
-    : Gets details of all users like firstname, lastname and id in an array format. Returned object should have a key called users which contains the list of all users with their email/firstname/lastname.
+    Description: Gets details of all users like firstname, lastname and id in an array format. Returned object should have a key called users which contains the list of all users with their email/firstname/lastname.
     The users username and password should be fetched from the headers and checked before the array is returned
-    Response: Description200 OK with the protected data in JSON format if the username and password in headers are valid, or 401 Unauthorized if the username and password are missing or invalid.
+    Response: 200 OK with the protected data in JSON format if the username and password in headers are valid, or 401 Unauthorized if the username and password are missing or invalid.
     Example: GET http://localhost:3000/data
 
   - For any other route not defined in the server return 404
@@ -29,26 +29,79 @@
   Testing the server - run `npm run test-authenticationServer` command in terminal
  */
 
-const express = require("express")
-const PORT = 3000;
-const app = express();
-// write your logic here, DONT WRITE app.listen(3000) when you're running tests, the tests will automatically start the server
-var users = [];
-app.use(express.json())
-app.post("/signup", (req, res) => {
-  var user = req.body;
-  let userAlreadyExists = false;
-  for(let i =0; i < users.length; i++) {
-    if(user[i].email === user.email) {
-      userAlreadyExists = true;
-      break;
+  const express = require("express")
+  const PORT = 3000;
+  const app = express();
+  // write your logic here, DONT WRITE app.listen(3000) when you're running tests, the tests will automatically start the server
+  
+  var users = [];
+  
+  app.use(express.json());
+  app.post("/signup", (req, res) => {
+    var user = req.body;
+    let userAlreadyExists = false;
+    for (var i = 0; i<users.length; i++) {
+      if (users[i].email === user.email) {
+          userAlreadyExists = true;
+          break;
+      }
     }
-  }
-  if(userAlreadyExists) {
-    res.sendStatus(400)
-  } else {
-    res.send(200).send("Signup Successfully");
-  }
-})
-
-module.exports = app;
+    if (userAlreadyExists) {
+      res.sendStatus(400);
+    } else {
+      users.push(user);
+      res.status(201).send("Signup successful");
+    }
+  });
+  
+  app.post("/login", (req, res) => {
+    var user = req.body;
+    let userFound = null;
+    for (var i = 0; i<users.length; i++) {
+      if (users[i].email === user.email && users[i].password === user.password) {
+          userFound = users[i];
+          break;
+      }
+    }
+  
+    if (userFound) {
+      res.json({
+          firstName: userFound.firstName,
+          lastName: userFound.lastName,
+          email: userFound.email
+      });
+    } else {
+      res.sendStatus(401);
+    }
+  });
+  
+  app.get("/data", (req, res) => {
+    var email = req.headers.email;
+    var password = req.headers.password;
+    let userFound = false;
+    for (var i = 0; i<users.length; i++) {
+      if (users[i].email === email && users[i].password === password) {
+          userFound = true;
+          break;
+      }
+    }
+  
+    if (userFound) {
+      let usersToReturn = [];
+      for (let i = 0; i<users.length; i++) {
+          usersToReturn.push({
+              firstName: users[i].firstName,
+              lastName: users[i].lastName,
+              email: users[i].email
+          });
+      }
+      res.json({
+          users
+      });
+    } else {
+      res.sendStatus(401);
+    }
+  });
+  
+  module.exports = app;
+  
