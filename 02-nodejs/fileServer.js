9509fbc98faad02caf2ceaf1694919f98b6cfbd4
Divyanshu Rathore <divyanshu03@live.com>
2023-06-26 23:42:18 +0530

    Completes all the tasks

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -19,7 +19,71 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const app = express();
 
+
+const app = express();
+const DIR_NAME = path.join(__dirname, 'files');
+
+
+function readDirContents() {
+  return new Promise((resolve, reject) => {
+    fs.readdir(DIR_NAME, (err, files) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(files);
+      }
+    });
+  })
+}
+
+function readFile(filename) {
+  filename = path.join(__dirname, 'files', filename);
+  // console.log(filename);
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, 'utf8', (err, data) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          resolve();
+        } else {
+          reject(err);
+        }
+      } else {
+        resolve(data);
+      }
+    });
+  })
+}
+
+app.get('/files', (req, res) => {
+  readDirContents()
+    .then(files => {
+      res.status(200).json(files);
+    })
+    .catch(err => {
+      res.status(500).send('Unable to read files due to internal server error');
+    });
+});
+
+app.get('/file/:filename', (req, res) => {
+  const name = req.params.filename;
+
+  readFile(name)
+    .then(data => {
+      if (data) {
+        res.status(200).send(data);
+      } else {
+        res.status(404).send('File not found');
+      }
+    })
+    .catch(err => {
+      res.status(500).send('Unable to read file due to internal server error');
+    });
+});
+
+// Any other route is not found
+app.use('*', (req, res) => {
+  res.status(404).send('Route not found');
+});
 
 module.exports = app;
--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -39,11 +39,229 @@
 
   Testing the server - run `npm run test-todoServer` command in terminal
  */
+
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const DATA_FILE = path.join(__dirname, 'files', 'data.json');
+
 app.use(bodyParser.json());
+
+// Middleware to handle bodyParser errors
+app.use((err, req, res, next) => {
+  if (err) {
+    res.status(400).send('Bad Request');
+  } else {
+    next();
+  }
+})
+
+function generateId() {
+  return Math.random().toString(36).slice(2);
+}
+
+function findById(id, todos) {
+  return todos.find(record => record.id === id);
+}
+
+function readToDoList() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(DATA_FILE, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+function addToDo(todoItem, todoList) {
+  return new Promise((resolve, reject) => {
+    const id = generateId();
+    todoItem.id = id;
+    todoList.push(todoItem);
+
+    fs.writeFile(DATA_FILE, JSON.stringify(todoList), 'utf8', (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        const objId = { id: todoItem.id };
+
+        resolve(objId);
+      }
+    });
+  });
+}
+
+function updateToDo(id, body, todoList) {
+  return new Promise((resolve, reject) => {
+    const itemIndex = todoList.findIndex(record => record.id === id);
+    let todoItem = todoList[itemIndex];
+
+    if (todoItem) {
+      Object.assign(todoItem, body);
+      todoList[itemIndex] = todoItem;
+
+      fs.writeFile(DATA_FILE, JSON.stringify(todoList), 'utf8', (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(todoItem);
+        }
+      });
+    } else {
+      resolve(todoItem);
+    }
+  });
+}
+
+function deleteToDo(id, todoList) {
+  return new Promise((resolve, reject) => {
+    const itemIndex = todoList.findIndex(record => record.id === id);
+
+    if (itemIndex === -1) {
+      resolve(itemIndex);
+    } else {
+      todoList.splice(itemIndex, 1);
+
+      fs.writeFile(DATA_FILE, JSON.stringify(todoList), 'utf8', (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(itemIndex);
+        }
+      });
+    }
+  })
+}
+
+function parseData(jsonData) {
+  return new Promise((resolve, reject) => {
+    try {
+      const parsedData = JSON.parse(jsonData);
+
+      resolve(parsedData);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+// Get a list of todos
+app.get('/todos', (req, res) => {
+  readToDoList()
+  .then(parseData)
+  .then(parsedDataObject => {
+    res.status(200).json(parsedDataObject);
+  })
+  .catch(error => {
+    res.status(500).send("Couldn't read the list due to an internal server error");
+  });
+});
+
+// Add a new todo item in todo list
+app.post('/todos/', (req, res) => {
+  const todoBody = req.body;
+
+  readToDoList()
+    .then(parseData)
+    .then(parsedDataObject => {
+      const todos = parsedDataObject;
+
+      addToDo(todoBody, todos)
+        .then(objId => {
+          res.status(201).json(objId);
+        })
+        .catch(error => {
+          res.status(500).send("Couldn't save to disk due to an internal server error");
+        });
+    })
+    .catch(error => {
+      res.status(500).send("Couldn't read the list due to an internal server error");
+    });
+});
+
+// Fetch todo item
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  readToDoList()
+    .then(parseData)
+    .then(parsedDataObject => {
+      const todo = findById(id, parsedDataObject);
+
+      if (todo) {
+        res.status(200).json(todo);
+      } else {
+        res.status(404).send('Todo not found');
+      }
+    })
+    .catch(error => {
+      res.status(500).send("Couldn't read the list due to an internal server error");
+    });
+});
+
+// Update todo item
+app.put('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const todoBody = req.body;
+
+  readToDoList()
+    .then(parseData)
+    .then(parsedDataObject => {
+      const todos = parsedDataObject;
+
+      updateToDo(id, todoBody, todos)
+        .then(todo => {
+          if (todo) {
+            res.status(200).json(todo);
+          } else {
+            res.status(404).send('Todo not found');
+          }
+        })
+        .catch(error => {
+          res.status(500).send("Couldn't save to disk due to an internal server error");
+        });
+    })
+    .catch(error => {
+      res.status(500).send("Couldn't read the list due to an internal server error");
+    });
+});
+
+// Delete todo item
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  readToDoList()
+    .then(parseData)
+    .then(parsedDataObject => {
+      const todos = parsedDataObject;
+
+      deleteToDo(id, todos)
+        .then(indexTodo => {
+          if (indexTodo === -1) {
+            res.status(404).send('Todo not found');
+          } else {
+            res.status(200).send('Todo deleted successfully.');
+          }
+        })
+        .catch(error => {
+          res.status(500).send("Couldn't save to disk due to an internal server error");
+        });
+    })
+    .catch(error => {
+      res.status(500).send("Couldn't read the list due to an internal server error");
+    });
+});
+
+// Any other route is not found
+app.use('*', (req, res) => {
+  res.status(404).send('Route not found');
+});
 
 module.exports = app;
