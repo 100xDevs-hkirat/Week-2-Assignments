153afbec6bfa199ee91c16a6a97b9eeeb9cae5f7
krishna <krishnavm143@gmail.com>
2023-06-26 05:49:54 +0530

    completed todoapp

--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -41,9 +41,71 @@
  */
 const express = require('express');
 const bodyParser = require('body-parser');
+const port = 3000;
 
 const app = express();
 
 app.use(bodyParser.json());
+
+let todos = [
+  {
+    id: 123,
+    title: "table",
+    description: "nothing"
+  }
+]
+
+app.get('/todos', (req, res) => {
+  res.status(200).json(todos)
+})
+app.get('/todos/:id', (req, res) => {
+  const todo = todos.find(t => t.id === parseInt(req.params.id))
+  if (!todo) {
+    res.status(404).json({ "err": "no data found" })
+  } else {
+    res.json({ todo })
+  }
+})
+
+app.post('/todos', (req, res) => {
+  const newTodo = {
+    id: Math.floor(Math.random() * 100000),
+    title: req.body.title,
+    description: req.body.description
+  }
+  todos.push(newTodo)
+  res.status(201).json(newTodo)
+})
+
+app.put('/todos/:id', (req, res) => {
+  const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id))
+  if (todoIndex === -1) {
+    res.status(404).json({
+      error: "not Found"
+    })
+  } else {
+    todos[todoIndex].title = req.body.title
+    todos[todoIndex].description = req.body.description
+    res.status(200).json(todos[todoIndex])
+  }
+})
+app.delete('/todos/:id', (req, res) => {
+  const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id))
+  if (todoIndex === -1) {
+    res.status(404).json({
+      error: "not Found"
+    })
+  } else {
+    // todos[todoIndex].title = req.body.title
+    // todos[todoIndex].description = req.body.description
+    // res.status(200).json(todos[todoIndex])
+    todos.splice(todoIndex, 1)
+    res.status(200).json(todos)
+  }
+})
+function starter() {
+  console.log(`App running at port : ${port}`)
+}
+app.listen(port, starter)
 
 module.exports = app;
