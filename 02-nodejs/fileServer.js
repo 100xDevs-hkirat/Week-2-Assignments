40e718101d80f78b9bfa7b3a6227d58a1cff5793
shamshadalamme <sashamshadalam977@gmail.com>
2023-06-29 17:16:10 +0200

    Solved and pushed

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -16,10 +16,32 @@
 
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
+app.get("/files", (req, res) => {
+  fs.readdir(path.join(__dirname, "./files/"), (err, files) => {
+    if (err) return res.status(500).json({ error: "Error reading file" });
+    res.json(files);
+  });
+});
 
+app.get("/files/:filename", (req, res) => {
+  const filePath = path.join(__dirname, "./files/", req.params.filename);
+
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) return res.status(404).send("File not found");
+    res.send(data);
+  });
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Route not found");
+});
+
+app.listen(3000, () => {
+  console.log("Listening at 3000");
+});
 module.exports = app;
