e359d9ccde596c822aa1496f6d575a6ed70df3a9
NooBIE-Nilay <nbanerjee02.asn@gmail.com>
2023-06-29 11:00:56 +0530

    Solved All Problems of Week-2-Assignments

--- 02-nodejs/authenticationServer.js
+++ 02-nodejs/authenticationServer.js
@@ -19,9 +19,9 @@
     Example: POST http://localhost:3000/login
 
   3. GET /data - Fetch all user's names and ids from the server (Protected route)
-    Description: Gets details of all users like firstname, lastname and id in an array format. Returned object should have a key called users which contains the list of all users with their email/firstname/lastname.
+    Description: Gets details of all users like firstname, lastname and id in an array format. Returned object should have a key called users which contains the list of all users with their username/firstname/lastname.
     The users username and password should be fetched from the headers and checked before the array is returned
-    Response: 200 OK with the protected data in JSON format if the username and password in headers are valid, or 401 Unauthorized if the username and password are missing or invalid.
+    Response: 200 OK with the protected data in JSON format if the username and password in headers are valid, or 401 Unauthorized if the email and password are missing or invalid.
     Example: GET http://localhost:3000/data
 
   - For any other route not defined in the server return 404
@@ -29,9 +29,63 @@
   Testing the server - run `npm run test-authenticationServer` command in terminal
  */
 
-const express = require("express")
+const express = require("express");
+const bodyParser = require("body-parser");
 const PORT = 3000;
 const app = express();
-// write your logic here, DONT WRITE app.listen(3000) when you're running tests, the tests will automatically start the server
-
+app.use(bodyParser.json());
+const users = [];
+app.post("/signup", (req, res) => {
+  const userIndex = users.findIndex((user) => user.email === req.body.email);
+  console.log(users);
+  if (userIndex == -1) {
+    const newUser = {
+      id: Math.floor(Math.random() * 10000),
+      email: req.body.email,
+      password: req.body.password,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    };
+    users.push(newUser);
+    res.status(201).send("Signup successful");
+  } else res.status(400).send("Username already exists");
+});
+app.post("/login", (req, res) => {
+  const userIndex = users.findIndex(
+    (user) =>
+      user.email === req.body.email && user.password === req.body.password
+  );
+  if (userIndex < 0) res.status(401).send("Unauthorized");
+  else {
+    const responseObj = {
+      id: users[userIndex].id,
+      email: users[userIndex].email,
+      firstName: users[userIndex].firstName,
+      lastName: users[userIndex].lastName,
+    };
+    res.status(200).json(responseObj);
+  }
+});
+app.get("/data", (req, res) => {
+  const userIndex = users.findIndex((user) => {
+    return (
+      user.email === req.headers.email && user.password === req.headers.password
+    );
+  });
+  if (userIndex < 0) res.status(401).send("Unauthorized");
+  else {
+    const responseArrObj = { users: [] };
+    users.forEach((user) => {
+      const responseObj = {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+      };
+      responseArrObj.users.push(responseObj);
+    });
+    res.status(200).json(responseArrObj);
+  }
+});
+app.use((req, res, next) => res.status(404).send("Route not found"));
+// app.listen(PORT, () => console.log(`App started at port ${PORT}`));
 module.exports = app;
