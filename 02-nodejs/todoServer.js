439fee818fa9c0a3636b89d7fcfd73a2b7a7a4f9
=rahulcode60 <rahulcode60@gmail.com>
2023-07-03 22:43:45 +0530

    week2 assignment solved

--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -46,4 +46,94 @@ const app = express();
 
 app.use(bodyParser.json());
 
+
+//easy todo
+let todos =[];
+
+//function check index in todos array
+function findIdx(arr,id){
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].id === id) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+
+function removeAtIndex(arr, index) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== index) newArray.push(arr[i]);
+  }
+  return newArray;
+}
+
+
+//get todo
+app.get('/todos',(req,res)=>{
+  res.json(todos);
+});
+
+//get todo by id
+app.get('/todos/:id',(req,res)=>{
+  const idx = findIdx(todos,parseInt(req.params.id));
+
+  if(idx === -1){
+    res.status(404).send();
+  }
+  else{
+    res.json(todos[idx]);
+  }
+
+});
+
+//POST /todos - Create a new todo item
+
+app.post('/todos',(req,res)=>{
+    const newTodo = {
+      id:Math.floor(Math.random() *1000),
+      title: req.body.title,
+      description: req.body.description
+    };
+
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+});
+
+
+//PUT /todos/:id
+app.put('/todos/:id',(req,res)=>{
+  const idx = findIdx(todos,parseInt(req.params.id));
+
+  if(idx === -1){
+    res.status(404).send();
+  }
+  else{
+    todos[idx].title = req.body.title;
+    todos[idx].description = req.body.description;
+    res.json(todos[idx]);
+  }
+});
+
+
+//DELETE /todos/:id
+app.delete('/todos/:id',(req,res)=>{
+  const idx = findIdx(todos,parseInt(req.params.id));
+
+  if(idx === -1){
+    res.status(404).send();
+  }
+  else{
+    todos = removeAtIndex(todos,idx);
+    res.status(200).send();
+  }
+});
+
+// for all other routes, return 404
+app.use((req, res, next) => {
+  res.status(404).send();
+});
+
+
 module.exports = app;
