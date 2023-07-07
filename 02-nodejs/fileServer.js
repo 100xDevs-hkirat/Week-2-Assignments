7ea51fd40299b87864a81da8a17135d82d28f9c8
harishkevin <89762850+harishkevin@users.noreply.github.com>
2023-07-07 15:56:03 +0530

    fileserver completed

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -21,5 +21,26 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+app.get('/files', (req, res) => {
+  fs.readdir('./files', 'utf-8', (err, data) => {
+    if (err) res.sendStatus(404);
+    else res.json(data);
+  })
+})
+
+app.get('/file/:filename', (req, res) => {
+  const filePath = path.join(__dirname, './files/', req.params.filename)
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(404).send('File not found');
+    }
+    res.send(data);
+  });
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send('Route not found')
+})
 
 module.exports = app;
