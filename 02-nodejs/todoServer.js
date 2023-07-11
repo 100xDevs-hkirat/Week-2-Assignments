45914f5d5713c85d5400395c93399cb7df6ef5fc
manosatpathy <manosatpathy@gmail.com>
2023-07-11 20:55:46 +0530

    finishing todoServer.js

--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -39,11 +39,60 @@
 
   Testing the server - run `npm run test-todoServer` command in terminal
  */
-const express = require('express');
-const bodyParser = require('body-parser');
-
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.json());
+
+let todos = [];
+
+app.get("/todos", (req, res) => {
+  res.send(todos);
+});
+
+app.get("/todos/:id", (req, res) => {
+  const todo = todos.find((e) => e.id === parseInt(req.params.id));
+  if (!todo) {
+    res.status(404).send();
+  } else {
+    res.json(todo);
+  }
+});
+
+app.post("/todos", (req, res) => {
+  const newTodo = {
+    id: Math.floor(Math.random() * 1000),
+    title: req.body.title,
+    description: req.body.description,
+  };
+  todos.push(newTodo);
+  res.status(200).json(newTodo);
+});
+
+app.put("/todos/:id", (req, res) => {
+  const todoindex = todos.findIndex((t) => t.id === parseInt(req.params.id));
+  if (todoindex < 0) {
+    res.status(404).send();
+  } else {
+    todos[todoindex].title = req.body.title;
+    todos[todoindex].description = req.body.description;
+    res.json(todos[todoindex]);
+  }
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const todoindex = todos.findIndex((t) => t.id === parseInt(req.params.id));
+  if (todoindex < 0 ) {
+    res.status(404).send();
+  } else {
+    todos.splice(todoindex, 1);
+    res.status(200).send();
+  }
+});
+
+app.listen(3000, () => {
+  console.log("app is running");
+});
 
 module.exports = app;
