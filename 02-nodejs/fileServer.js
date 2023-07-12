e40b62e86c4066084075630fe71968009b867f11
Shashwat Singh <shashwat123student@gmail.com>
2023-07-12 19:32:31 +0530

    Added Solution for FileServer

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -21,5 +21,26 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+app.get("/files",(req,res)=>{
+    fs.readdir(path.join(__dirname,'./files'), (err,files)=>{
+        if(err){
+            return res.sendStatus(500);
+        }
+        res.status(200).json(files);
+    });
+});
 
+app.get("/file/:filename", (req,res)=>{
+    const filePath = path.join(__dirname,'./files',req.params.filename);
+    fs.readFile(filePath,"utf8", (err,data)=>{
+        if(err)
+            res.status(404).send("File not found");
+        else
+            res.status(200).send(data);
+    });
+});
+
+app.all("*",(req,res)=>{
+    res.status(404).send("Route not found");
+})
 module.exports = app;
