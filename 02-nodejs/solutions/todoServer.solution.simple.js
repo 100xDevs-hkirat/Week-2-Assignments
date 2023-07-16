d5a0b85ae31485addcf857592c2c42084eb2403c
anas49 <anas@depasserinfotech.in>
2023-07-16 23:45:27 +0530

    Solution Added

--- 02-nodejs/solutions/todoServer.solution.simple.js
+++ 02-nodejs/solutions/todoServer.solution.simple.js
@@ -1,74 +1,117 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyPareser = require('body-parser')
+const fs = require('fs')
+const cors = require('cors');
 
-const app = express();
 
-app.use(bodyParser.json());
+const app = express()
+app.use(bodyPareser.json())
 
-let todos = [];
+app.use(cors());
 
-function findIndex(arr, id) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].id === id) return i;
-  }
-  return -1;
+function getIndexOfId(index,data)
+{
+	for (let i = 0; i < data.length; i++) 
+	{
+		if(data[i].id == index) return i;
+	}
+	return -1;
 }
 
-function removeAtIndex(arr, index) {
-  let newArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (i !== index) newArray.push(arr[i]);
+function removeIndex(index, arr) {
+	let newArray = [];
+	for (let i = 0; i < arr.length; i++) {
+	  if (i !== index) newArray.push(arr[i]);
+	}
+	return newArray;
   }
-  return newArray;
-}
 
-app.get('/todos', (req, res) => {
-  res.json(todos);
+app.post('/todos' , (req , res) => {
+	const newTodos = 
+	{
+		id : Math.floor(Math.random() * 1000000), // unique random id
+		title : req.body.title,
+		description : req.body.description
+	}
+
+	fs.readFile('./todos.json','utf-8',(err,data) =>{
+		var todos = [];
+		if(err) throw err;
+		if(data) 
+		{
+			todos = JSON.parse(data);
+		}
+		todos.push(newTodos)
+		fs.writeFile('./todos.json',JSON.stringify(todos),(err) =>{
+			if(err) throw err
+		});
+		res.status(201).json(newTodos)
+	});
 });
 
-app.get('/todos/:id', (req, res) => {
-  const todoIndex = findIndex(todos, parseInt(req.params.id));
-  if (todoIndex === -1) {
-    res.status(404).send();
-  } else {
-    res.json(todos[todoIndex]);
-  }
+app.get('/todos' , (req,res) =>{
+	fs.readFile('./todos.json','utf-8',(err,data) =>{
+		if(err) throw err;
+		if(!data)
+		{
+			res.send(("No Records Found"));
+			
+		}
+		else
+		{
+			res.json(JSON.parse(data));
+		}
+	});
 });
 
-app.post('/todos', (req, res) => {
-  const newTodo = {
-    id: Math.floor(Math.random() * 1000000), // unique random id
-    title: req.body.title,
-    description: req.body.description
-  };
-  todos.push(newTodo);
-  res.status(201).json(newTodo);
+app.delete('/todos/:id' , (req,res) =>{
+	let index = req.params.id
+	fs.readFile('./todos.json','utf-8',(err,data) =>{
+		if(err) throw err;
+		var oldData = JSON.parse(data);
+		var todos = [];
+		var isIndexAvailable = getIndexOfId(index,oldData);
+		if(isIndexAvailable === -1) return res.status(400).send("No Records Found");
+		todos = removeIndex(isIndexAvailable,oldData);
+		fs.writeFile('./todos.json',JSON.stringify(todos),(err) =>{
+			if(err) throw err;
+			res.send("Record Deleted").status(200);
+		})
+	});
 });
 
-app.put('/todos/:id', (req, res) => {
-  const todoIndex = findIndex(todos, parseInt(req.params.id));
-  if (todoIndex === -1) {
-    res.status(404).send();
-  } else {
-    todos[todoIndex].title = req.body.title;
-    todos[todoIndex].description = req.body.description;
-    res.json(todos[todoIndex]);
-  }
+app.put('/todos/:id',(req,res) =>{
+	var todos = [];
+	var index = req.params.id;
+	var updateData = 
+	{
+		title:req.body.title,
+		description:req.body.description
+	}
+	fs.readFile('./todos.json','utf-8',(err,data) =>{
+		if(err) throw err;
+		var oldData = JSON.parse(data);
+		var getIndexOfIds = getIndexOfId(index,oldData)
+		if(getIndexOfIds === -1) res.status(400).send("No Records Found")
+		for (let i = 0; i < oldData.length; i++) 
+		{
+			if(i === getIndexOfIds)
+			{
+				oldData[i].title = updateData.title,
+				oldData[i].description = updateData.description
+				todos.push(oldData)
+			}
+			else
+			{
+				todos.push(oldData[i])
+			}
+		}
+		fs.writeFile('./todos.json',JSON.stringify(todos),(err) =>{
+			if(err) throw err
+		});
+		res.status(200).send("Record Updated Successfully")
+		
+	});
 });
 
-app.delete('/todos/:id', (req, res) => {
-  const todoIndex = findIndex(todos, parseInt(req.params.id));
-  if (todoIndex === -1) {
-    res.status(404).send();
-  } else {
-    todos = removeAtIndex(todos, todoIndex);
-    res.status(200).send();
-  }
-});
-
-// for all other routes, return 404
-app.use((req, res, next) => {
-  res.status(404).send();
-});
-
-module.exports = app;
+app.listen(3000)
