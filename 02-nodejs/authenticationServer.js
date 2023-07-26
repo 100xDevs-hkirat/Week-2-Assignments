749fa2f83e9ac8d165bb00409ccd29469480fada
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-07-26 15:16:34 +0530

    authenticationServer.js

--- 02-nodejs/authenticationServer.js
+++ 02-nodejs/authenticationServer.js
@@ -29,9 +29,52 @@
   Testing the server - run `npm run test-authenticationServer` command in terminal
  */
 
-const express = require("express")
+const express = require("express");
 const PORT = 3000;
 const app = express();
 // write your logic here, DONT WRITE app.listen(3000) when you're running tests, the tests will automatically start the server
+var users = [];
 
-module.exports = app;
+app.use(express.json());
+app.post("/signup", (req, res) => {
+	var user = req.body;
+	let userExists = false;
+	for (var i = 0; i < users.length; i++) {
+		if (users[i].email === user.email) {
+			userExists = true;
+			break;
+		}
+	}
+	if (userExists) {
+		res.sendStatus(400);
+	} else {
+		users.push(user);
+		res.status(201).send("Signup successful");
+	}
+});
+
+app.post("/login", (req, res) => {
+	var user = req.body;
+	let userFound = null;
+	for (var i = 0; i < users.length; i++) {
+		if (users[i].email === user.email && users[i].password === user.password) {
+			userFound = users[i];
+			break;
+		}
+	}
+	if (userFound) {
+		res.json({
+			firstName: userFound.firstName,
+			lastName: userFound.lastName,
+			email: userFound.email,
+		});
+	} else {
+		res.sendStatus(404);
+	}
+});
+
+app.listen(PORT, () => {
+	console.log("Server created successfully at port 3000");
+});
+
+// module.exports = app;
