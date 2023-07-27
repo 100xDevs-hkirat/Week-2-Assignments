6294677d3369a9f520f2ab9e28cf75f42989c7a8
Vibhor <vibgitcode27@gmail.com>
2023-07-28 00:37:27 +0530

    solution of fileServer and todoServer

---
+++ 02-nodejs/demo.js
@@ -0,0 +1,17 @@
+let one = {
+    id : 1 , 
+    description : "Vibhor"
+}
+
+console.log(one)
+
+let id = 2
+let description = "Rahul"
+let dem = {
+    of : 12,
+    cription : `Rahul`
+}
+
+Object.assign(one , dem)
+
+console.log(one)
