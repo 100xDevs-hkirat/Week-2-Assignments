8ef54afa587adfc2eb80948f2b312bb538be5f22
sujit raul <sujit.raul@rsmus.com>
2023-07-29 23:15:13 +0530

    solution weeek - 2

--- 02-nodejs/authenticationServer.js
+++ 02-nodejs/authenticationServer.js
@@ -7,7 +7,8 @@
 
   The expected API endpoints are defined below,
   1. POST /signup - User Signup
-    Description: Allows users to create an account. This should be stored in an array on the server, and a unique id should be generated for every new user that is added.
+    Description: Allows users to create an account. This should be stored in an array on the server, 
+    and a unique id should be generated for every new user that is added.
     Request Body: JSON object with username, password, firstName and lastName fields.
     Response: 201 Created if successful, or 400 Bad Request if the username already exists.
     Example: POST http://localhost:3000/signup
@@ -19,9 +20,13 @@
     Example: POST http://localhost:3000/login
 
   3. GET /data - Fetch all user's names and ids from the server (Protected route)
-    Description: Gets details of all users like firstname, lastname and id in an array format. Returned object should have a key called users which contains the list of all users with their email/firstname/lastname.
-    The users username and password should be fetched from the headers and checked before the array is returned
-    Response: 200 OK with the protected data in JSON format if the username and password in headers are valid, or 401 Unauthorized if the username and password are missing or invalid.
+    Description: Gets details of all users like firstname, lastname and id in an array format. 
+    Returned object should have a key called users which contains the list of all users with their 
+    email/firstname/lastname.
+    The users username and password should be fetched from the headers and checked before the array is 
+    returned
+    Response: 200 OK with the protected data in JSON format if the username and password in headers are 
+    valid, or 401 Unauthorized if the username and password are missing or invalid.
     Example: GET http://localhost:3000/data
 
   - For any other route not defined in the server return 404
@@ -32,6 +37,78 @@
 const express = require("express")
 const PORT = 3000;
 const app = express();
-// write your logic here, DONT WRITE app.listen(3000) when you're running tests, the tests will automatically start the server
+// write your logic here, DONT WRITE app.listen(3000) when you're running tests, the tests will 
+//automatically start the server
+
+var users=[];
+
+app.use(express.json());
+
+app.post("/signup",(req,res)=>{
+    var user = req.body;
+    let userAlreadyExists = false;
+    for (var i=0;i<users.length;i++){
+      if(users[i].email === user.email){
+        userAlreadyExists = true;
+        break;
+      }
+    }
+    if(userAlreadyExists){
+      res.sendStatus(400);
+    } else {
+      users.push(user);
+      res.status(201).send("Signup successful");
+    }
+});
+
+app.post("/login", (req, res) => {
+  var user = req.body;
+  let userFound = null;
+  for (var i = 0; i<users.length; i++) {
+    if (users[i].email === user.email && users[i].password === user.password) {
+        userFound = users[i];
+        break;
+    }
+  }
+
+  if (userFound) {
+    res.json({
+        firstName: userFound.firstName,
+        lastName: userFound.lastName,
+        email: userFound.email
+    });
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+app.get('/data',(req,res)=>{
+    var email = req.headers.email
+    var password = req.headers.password
+    let userFound = false;
+    for (var i=0;i<users.length;i++){
+      if(users[i].email === email && users[i].password===password){
+        userFound = true;
+        break;
+      }
+    }
+
+    if(userFound){
+      let userToReturn = [];
+      for(i=1;i<users.length;i++){
+         usersToReturn.push({
+          firstName: users[i].firstName,
+          lastName: users[i].lastName,
+          email: users[i].email
+         })
+
+      }
+      res.json({users});
+
+    }
+    else {
+      res.sendStatus(401);
+    }
+});
 
 module.exports = app;
