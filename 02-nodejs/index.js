0b9dfd040281aaff228e580f36f4285eeb0a26f7
Akash Shaw <shawakash2003@gmail.com>
2023-08-03 22:56:24 +0530

    Deleting without reloading

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -42,11 +42,12 @@
 const express = require('express');
 const fs = require("fs");
 const bodyParser = require('body-parser');
-
+const cors = require("cors");
 const app = express();
 const port = 3005;
 
 app.use(bodyParser.json());
+app.use(cors())
 
 
 // const readFile = () => {
@@ -177,9 +178,9 @@ app.delete('/todos/:id', (req, res) => {
 // })
 
 // for all other routes, return 404
-app.use((req, res, next) => {
-  res.status(404).send();
-});
+// app.use((req, res, next) => {
+//   res.status(404).send();
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
