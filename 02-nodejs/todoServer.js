300f609fd207102992330e6f76bcde4b0e132998
Prathamesh Pawar <cm.b26.prathameshpawar@gmail.com>
2023-08-04 16:00:35 +0530

    Completed with tests

--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -39,11 +39,115 @@
 
   Testing the server - run `npm run test-todoServer` command in terminal
  */
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
 const app = express();
-
 app.use(bodyParser.json());
+const path = "db.json";
 
+let currentId = 1;
+function FindLength()
+{
+  data = fs.readFile(path, 'utf8', (err, data)=>{
+    const database = JSON.parse(data);
+    return database.length > 0 ? database.length : 1;
+  });
+}
+currentId = FindLength();
+
+function readData()
+{
+  const data = fs.readFileSync(path, 'utf8');
+  return JSON.parse(data);
+}
+function writeData(data) {
+  fs.writeFileSync(path, JSON.stringify(data, null, 2));
+}
+
+function create(req, res) {
+  console.log(currentId);
+  const newItem = {
+    id: currentId,
+    title: req.body.title,
+    completed: req.body.title || false,
+    description: req.body.description || ''
+  };
+
+  fs.readFile(path, 'utf8', (err, data)=>{
+    if(err) throw err;
+    const database = JSON.parse(data);
+    database.push(newItem);
+    fs.writeFile(path, JSON.stringify(database), (err)=>{
+      if(err) throw err;
+      currentId++;
+      res.status(201).json({
+        'id': currentId-1});
+    });
+  });
+}
+
+function getAll(req, res) {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if(err) throw err;
+    res.json(JSON.parse(data));
+  });
+}
+
+function getOne(req, res) {
+  const database = readData();
+  const id = parseInt(req.params.id);
+  const present = database.find((item) => item.id === id);
+  if(present)
+  {
+    res.json(present);
+  }
+  res.status(404).send();
+}
+
+function deleteOne(req, res) {
+  const database = readData();
+  const id = parseInt(req.params.id);
+  const present = database.find((item) => item.id === id);
+  if(present)
+  {
+    const databaseNew = database.filter((item) => item.id !== id);
+    writeData(databaseNew);
+    res.send();
+  }
+  res.status(404).send();
+}
+
+function deleteAll(req, res) {
+  writeData('');
+}
+
+function edit(req, res) {
+  const database = readData();
+  const idx = parseInt(req.params.id);
+  const present =  database.find(item => item.id === idx);
+  if(present)
+  {
+    present.title = req.body.title || present.title;
+    present.completed = req.body.completed || present.completed,
+    present.description = req.body.description || present.completed
+
+    writeData(database);
+    res.send();
+  }
+  res.status(404).send();
+}
+
+app.get("/todos", getAll);
+app.get("/todos/:id", getOne);
+app.post("/todos", create);
+app.put("/todos/:id", edit);
+app.delete("/todos/:id", deleteOne);
+app.delete('/todos/all', deleteAll);
+
+// export default app;
 module.exports = app;
+// app.listen(3000, () => {
+//   console.log("listening");
+// });
