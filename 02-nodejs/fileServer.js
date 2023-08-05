ca6aca0fae0831f0411b46dfff5e7ec222e6dddf
Aditya kumar <98655260+164adityakumar@users.noreply.github.com>
2023-08-05 18:12:47 +0530

    solved all todos

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -16,10 +16,41 @@
 
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const app = express();
-
+    const express = require('express');
+    const fs = require('fs');
+    const path = require('path');
+    const app = express();
+    
+    const pathToFiles = path.join(__dirname, './files');
+    
+    app.get('/files', (req, res) => {
+      fs.readdir(pathToFiles, (err, files) => {
+        if (err) {
+          console.error('Error reading directory:', err);
+          return res.status(500).json({ error: 'Failed to retrieve files' });
+        } else {
+          console.log('List of files in the directory:');
+          console.log(files);
+          res.json(files);
+        }
+      });
+    });
+    
+    app.get('/file/:filename', (req, res) => {
+      const filename = req.params.filename;
+      const filePath = path.join(pathToFiles, filename);
+      
+      fs.readFile(filePath, 'utf8', (err, content) => {
+        if (err) {
+          console.error('Error reading file:', err);
+          return res.status(404).send('File not found');
+        } else {
+          res.status(200).send(content);
+        }
+      });
+    });
+    
+  app.use((req, res) => { res.status(404).send('Route not found'); });
+// fs.readdir(pathToFiles,callbackfn);
 
 module.exports = app;
