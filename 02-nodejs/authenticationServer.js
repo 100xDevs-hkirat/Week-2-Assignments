e9242ee3b5016c04355553c0ef833232177d38e5
Krishna vamsi <k.durga@f5.com>
2023-08-21 00:09:18 +0530

    Added authenticationServer.js(Test's not working yet)

--- 02-nodejs/authenticationServer.js
+++ 02-nodejs/authenticationServer.js
@@ -32,6 +32,106 @@
 const express = require("express")
 const PORT = 3000;
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+
+
 // write your logic here, DONT WRITE app.listen(3000) when you're running tests, the tests will automatically start the server
+
+var userData = [];
+
+app.post("/signup", (req, res) => {
+  var currentUser = req.body;
+
+  var checkIfExists = false;
+
+  for(var i = 0; i < userData.length; i++) {
+    if(userData[i].email === currentUser.email) {
+      checkIfExists = true;
+      break;
+    }
+  }
+
+  if(checkIfExists === true) {
+    res.status(400).send("User Already Exists With this Username !! ")
+  }
+  else {
+    const uniqueId = Date.now();
+    currentUser["id"] = uniqueId;
+    userData.push(currentUser);
+    res.status(201).send("User Added !");
+  }
+
+  console.log(currentUser);
+})
+
+app.post("/login", (req, res) => {
+  var currentUser = req.body;
+  console.log(currentUser);
+
+  var validUser = false;
+  var user = {};
+
+  for(var i = 0; i < userData.length; i++) {
+    if((userData[i].email === currentUser.email) && (userData[i].password === currentUser.password)) {
+      // console.log(userData[i].username, userData[i].password);
+      validUser = true;
+      user = userData[i];
+      break;
+    }
+  }
+
+  // console.log(validUser);
+
+  if(validUser) {
+    const data = {id : user.id}
+    res.status(200).send(data);
+  }
+  else {
+    res.status(401).send("Authentication Failed");
+  }
+})
+
+app.get("/data", (req, res) => {
+  var currentUser = req.headers;
+
+  var validUser = false;
+
+  for(var i = 0; i < userData.length; i++) {
+    if(userData[i].email === currentUser.email && userData[i].password === currentUser.password) {
+      validUser = true;
+    }
+  }
+
+  var data = [];
+
+  for(var i = 0; i < userData.length; i++) {
+    data.push({
+      "email" : userData[i].email,
+      "firstName" : userData[i].firstName,
+      "lastName" : userData[i].lastName,
+      "id" : userData[i].id
+    })
+  }
+
+  if(validUser) {
+    res.status(200).send({"users" : data});
+  }
+  else {
+    res.status(401).send("Un Authorized User!!");
+  }
+
+
+})
+
+app.get("/", (req, res) => {
+  res.send("Authentication Server Initial Route !! ")
+})
+
+// app.listen(PORT, () => {
+//   console.log(`Example app listening on port ${PORT}`)
+// })
 
 module.exports = app;
