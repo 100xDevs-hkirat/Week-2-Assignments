472a25a57ff27f7f8208634dc76c0f2534102f40
Harsh Jha <hjha0695@gmail.com>
2023-09-10 12:48:43 +0530

    Done With fileServer.js

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -21,5 +21,36 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+const filesDir = path.join(__dirname, './files');
+
+
+app.get('/files', (req, res) => {
+  fs.readdir(filesDir, (err, files) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to retrieve files' });
+    } else {
+      res.status(200).json(files);
+    }
+  });
+})
+
+app.get('/file/:filename', (req, res) => {
+  const filepath = path.join(filesDir, req.params.filename);
+  fs.readFile(filepath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(404).send('File not found');
+    } else {
+      res.send(data);
+    }
+  });
+})
+
+app.use((req,res,next ) => {
+  res.status(404).send('Route not found')
+})
+
+// app.listen(3000 , () => {
+//   console.log('Server started on port 3000');
+// })
 
 module.exports = app;
